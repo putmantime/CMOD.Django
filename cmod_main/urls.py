1da8831b26174f3fb79424bfb5fd959fefd3b00f
Tim Putman <putmantime@gmail.com>
2016-10-11 13:13:37 -0700

    url for query string from wd oauth

--- cmod_main/urls.py
+++ cmod_main/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     url(r'^get_orgs', views.get_orgs, name='get_orgs'),
     url(r'^main_page/wd_go_edit', views.wd_go_edit, name='wd_go_edit'),
     url(r'^main_page/wd_credentials', views.wd_credentials, name='wd_credentials'),
-    url(r'^main_page/wd_oauth', views.wd_oauth, name='wd_oauth')
+    url(r'^main_page/wd_oauth', views.wd_oauth, name='wd_oauth'),
+    url(r'^main_page?oauth_verifier=(.+)&oauth_token=(.+)', views.wd_tokens, name='wd_tokens'),
+
 ]
