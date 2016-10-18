a6f282952a8aa920ac180cf0ea06dbb2ed634da7
Tim Putman <putmantime@gmail.com>
2016-10-17 21:07:50 -0700

    fixed nav-tab issue

--- cmod_main/urls.py
+++ cmod_main/urls.py
@@ -4,11 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-
     url(r'^main_page$', views.main_page, name='main_page'),
     url(r'^get_orgs', views.get_orgs, name='get_orgs'),
     url(r'^main_page/wd_go_edit', views.wd_go_edit, name='wd_go_edit'),
-    # url(r'^main_page/wd_credentials', views.wd_credentials, name='wd_credentials'),
     url(r'^main_page/wd_oauth', views.wd_oauth, name='wd_oauth'),
     url(r'^main_page/wd_oauth_deauth', views.wd_oauth_deauth, name='wd_oauth_deauth'),
 
