da1fdc715ddd9f3e30d2b2c4489cb72b08d6b0e8
Tim Putman <putmantime@gmail.com>
2016-11-09 11:55:55 -0800

    add operon form logic

--- cmod_main/urls.py
+++ cmod_main/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^main_page$', views.main_page, name='main_page'),
     url(r'^get_orgs', views.get_orgs, name='get_orgs'),
     url(r'^main_page/wd_go_edit', views.wd_go_edit, name='wd_go_edit'),
+    url(r'^main_page/wd_operon_edit', views.wd_operon_edit, name='wd_operon_edit'),
     url(r'^main_page/wd_oauth', views.wd_oauth, name='wd_oauth'),
     url(r'^main_page/wd_oauth_deauth', views.wd_oauth_deauth, name='wd_oauth_deauth'),
 
